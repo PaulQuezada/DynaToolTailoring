fb0728a28fe24997cd686ce83550c7f3731c5627
Paul Quezada <83780330+PaulQuezada@users.noreply.github.com>
2024-08-12 12:32:30 -0400

    Refactorizando importación del modelo de reglas de transformación

--- memoria/src/functions/importdata.ts
+++ memoria/src/functions/importdata.ts
@@ -1,5 +1,8 @@
 import { XMLParser } from "fast-xml-parser";
 
+
+// ********** Importar datos BPMN y el contexto en formato XMI **********
+
 // Creando la instancia del parser y del builder que se utilizará para procesar los archivos XML
 const parserOptions = {
     ignoreAttributes: false,
@@ -40,7 +43,7 @@ export async function fileUploadContext(xmlContext: string) {
             ? jsonObj["spcm:Context"].myDimensions
             : [jsonObj["spcm:Context"].myDimensions];
 
-       
+
         // Recorrer las dimensiones del contexto organizacional y obtener los atributos
         dimensions.forEach((dim: any) => {
             const contextAttributes = Array.isArray(dim.myContextAttributes)
@@ -51,8 +54,8 @@ export async function fileUploadContext(xmlContext: string) {
                 const values = Array.isArray(attr.posibleValues)
                     ? attr.posibleValues
                     : attr.posibleValues
-                      ? [attr.posibleValues]
-                      : [];
+                        ? [attr.posibleValues]
+                        : [];
                 const attributeData = {
                     Attribute: attr.name,
                     values: values.map((val: any) =>
@@ -88,4 +91,190 @@ export async function fileUploadBpmn(xmlBpmn: string) {
     }
     // Retorna los datos del archivo BPMN y los datos extraidos del archivo
     return task;
+}
+
+// ********** Importar modelo con reglas de transformación **********
+
+// Función para extraer los datos del archivo de reglas de transformación
+export function fileUploadTailoringModel(xmlModel: string) {
+    let isSuccessful: boolean = false; // Variable para almacenar si la carga del archivo fue exitosa
+    if (xmlModel !== "") {
+        // Variables para almacenar los datos extraidos del archivo
+        let attributesAndValues: any[] = [];
+        let onlyAttributes: any[] = [];
+        let activities: activity[] = [];
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlModel, "text/xml");
+
+        // Extraer los modelos de contexto, bpmn y reglas
+        const contextModelNode = xmlDoc.querySelector("ContextModel");
+        const bpmnModelNode = xmlDoc.querySelector("BPMNModel");
+        const rulesModelNode = xmlDoc.querySelector("RulesModel");
+
+        let contextModel = contextModelNode
+            ? contextModelNode.innerHTML.trim()
+            : "";
+        let bpmnModel = bpmnModelNode ? bpmnModelNode.innerHTML.trim() : "";
+        let rulesModel = rulesModelNode
+            ? rulesModelNode.innerHTML.trim()
+            : "";
+
+        // Le agregamos el encabezado a los modelos
+        contextModel = contextModel
+            ? `<?xml version="1.0" encoding="UTF-8"?>\n${contextModel}`
+            : "";
+        bpmnModel = bpmnModel
+            ? `<?xml version="1.0" encoding="UTF-8"?>\n${bpmnModel}`
+            : "";
+        rulesModel = rulesModel
+            ? `<?xml version="1.0" encoding="UTF-8"?>\n<RulesModel>${rulesModel}</RulesModel>`
+            : "";
+
+        // Verificar si el archivo tiene datos y si los tiene lo agregamos al localStorage
+        if (contextModel && bpmnModel && rulesModel) {
+            localStorage.setItem("xmlContext", contextModel);
+            localStorage.setItem("xmlBpmn", bpmnModel);
+            attributesAndValues = loadAtributtes()[0];
+            onlyAttributes = loadAtributtes()[1];
+            activities = convertRulesModel(rulesModel, onlyAttributes, attributesAndValues);
+            console.log("activities 2");
+            console.log(activities);
+            isSuccessful = true;
+        } else {
+            isSuccessful = false;
+        }
+
+        return [isSuccessful, activities];
+    }else{
+        return undefined;
+    }
+}
+
+
+// Función para extraer los atributos y valores de cada dimensión del archivo de reglas de transformación
+export function loadAtributtes(): any[] {
+    let xmlContext = localStorage.getItem("xmlContext")!;
+    const jsonObj = parser.parse(xmlContext);
+    console.log(jsonObj);
+    const dimensions = Array.isArray(jsonObj["spcm:Context"].myDimensions)
+        ? jsonObj["spcm:Context"].myDimensions
+        : [jsonObj["spcm:Context"].myDimensions];
+
+    // Usar una variable temporal para almacenar los atributos y valores de cada dimensión
+    let attributesAndValues: any[] = [];
+
+    dimensions.forEach((dim: any) => {
+        const contextAttributes = Array.isArray(dim.myContextAttributes)
+            ? dim.myContextAttributes
+            : [dim.myContextAttributes];
+
+        contextAttributes.forEach((attr: any) => {
+            const values = Array.isArray(attr.posibleValues)
+                ? attr.posibleValues
+                : attr.posibleValues
+                    ? [attr.posibleValues]
+                    : [];
+            const attributeData = {
+                Attribute: attr.name,
+                values: values.map((val: any) =>
+                    val.name ? val.name : "Unknown",
+                ),
+            };
+            attributesAndValues.push(attributeData);
+        });
+    });
+
+    // Extraer solo los atributos de la variable
+    let onlyAttributes: any[] = [];
+    attributesAndValues.forEach((data) => {
+        onlyAttributes.push(data.Attribute);
+    });
+
+    return [attributesAndValues, onlyAttributes];
+}
+
+// Función para convertir el XML de RulesModel a la variable "task" la cual se utiliza para mostrar todos los datos dentro de la herramienta
+export function convertRulesModel(xml: string, onlyAttributes: any[], attributesAndValues: any[]): activity[] {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xml, "text/xml");
+    const contentRules = xmlDoc.querySelectorAll("ContentRule");
+    let activities: activity[] = [];
+    console.log(xml);
+    contentRules.forEach((contentRule, index) => {
+        console.log(contentRule);
+        const activity: activity = {
+            id: index,
+            name: contentRule.getAttribute("name") || "",
+            subname: contentRule.getAttribute("subname") || "",
+            rules: parseRules(contentRule, onlyAttributes, attributesAndValues),
+            deleted: contentRule.getAttribute("deleted") === "true" ? true : contentRule.getAttribute("deleted") === "false" ? false : undefined,
+            replaced: contentRule.getAttribute("replace") ? true : undefined,
+            replaceActivity: contentRule.getAttribute("replace") || undefined,
+        };
+        activities.push(activity);
+    });
+    //taskNames.set(activities);
+    //localStorage.setItem("taskNames", JSON.stringify(activities));
+    console.log("activities 1");
+    console.log(activities);
+    return activities;
+}
+
+// Función para transformar las reglas descritas en el XML a un objeto de reglas que puede ser procesada por la herramienta
+function parseRules(element: Element, onlyAttributes: any[], attributesAndValues: any[]): Rule[] {
+    const rules: Rule[] = [];
+
+    // Iterar sobre todos los nodos Rule y ComplexRule directos de este elemento
+    element.childNodes.forEach((node) => {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            const ruleElement = node as Element;
+            const ruleType = ruleElement.tagName;
+
+            const id = ruleElement.getAttribute("id") || "";
+            const typeAttribute = ruleElement.getAttribute("type") || "";
+
+            console.log(ruleType);
+
+            if (ruleType === "Rule" || ruleType === "ComplexRule") {
+                if (typeAttribute === "Simple") {
+                    const simpleRule: SimpleRule = {
+                        id: id,
+                        type: "Simple",
+                        numberRule: 1,
+                        attribute:
+                            ruleElement.getAttribute("attribute") || "",
+                        value: ruleElement.getAttribute("value") || "",
+                        attributes: onlyAttributes,
+                        values: searchValues(
+                            ruleElement.getAttribute("attribute") || "", attributesAndValues
+                        )
+                    };
+                    rules.push(simpleRule);
+                } else if (typeAttribute === "Conector") {
+                    const connectorRule: ConnectorRule = {
+                        id: id,
+                        type: "Conector",
+                        logical_operator: ruleElement.getAttribute("attribute") || "Or", // Asumir un valor por defecto o ajustar según el XML
+                        logicals: ["And", "Or"],
+                    };
+                    rules.push(connectorRule);
+                } else if (ruleType === "ComplexRule") {
+                    const complexRule: ComplexRule = {
+                        id: id,
+                        type: "Complex",
+                        numberRule: 2,
+                        rules: parseRules(ruleElement, onlyAttributes, attributesAndValues), // Recursividad para manejar reglas anidadas
+                    };
+                    rules.push(complexRule);
+                }
+            }
+        }
+    });
+
+    return rules;
+}
+
+function searchValues(value: String, attributesAndValues: any[]): any[] {
+    return attributesAndValues.filter((attr) => attr.Attribute === value)[0]
+        .values;
 }
